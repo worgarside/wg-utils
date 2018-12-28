31a3b18e00153b7ee95e627f49fc6566aa29e31d
Will Garside <worgarside@gmail.com>
2018-12-28 17:11:56 +0000

    Updated log directory

--- scripts/cron/duck.sh
+++ scripts/cron/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=15rc&token=439fa757-cde5-46be-93d9-204701c0bc14&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=15rc&token=439fa757-cde5-46be-93d9-204701c0bc14&ip=" | curl -k -o ~/Projects/wg-utils/logs/duck.log -K -
